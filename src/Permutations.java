3b25d3fdf6d0c78e97f966c0f843a68d23aff255
Chandrasekar Srinivasan <chander1993@gmail.com>
2020-04-25 14:39:07 +0530

    Permutations recursion

---
+++ src/Permutations.java
@@ -0,0 +1,33 @@
+// PROBLEM : 46
+
+import java.util.*;
+
+ class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> tres = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        permute(nums, 0);
+        return res;
+    }
+    
+    public void permute(int[] nums, int index) {
+        if(tres.size() == nums.length) {
+            // a new list to be created and added because tres is a work array and will be modified.
+            res.add(new ArrayList<>(tres));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(tres.contains(nums[i])) continue;  // if the digit is already there in the list then continue.
+            
+            tres.add(nums[i]);
+            permute(nums, index + 1);
+            // clear the element once the work is over in the tree. 
+            tres.remove(tres.size() - 1);
+        }
+    }
+}
+
+public class Permutations {
+
+}
